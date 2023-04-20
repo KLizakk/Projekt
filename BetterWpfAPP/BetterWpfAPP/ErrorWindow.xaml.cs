87f5041eb814bcb99af7587ce2e639cafc0562d2
KLizakk <116660589+KLizakk@users.noreply.github.com>
2023-04-20 17:02:04 +0200

    Add files via upload

---
+++ BetterWpfAPP/BetterWpfAPP/ErrorWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BetterWpfAPP
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        public ErrorWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+       
+    }
+}
---
+++ BetterWpfAPP/BetterWpfAPP/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media.Media3D;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace BetterWpfAPP
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Wyszukaj_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            Wprowadzanie.Text = "";
+        }
+        //private void Wyszukaj_LostFocs(object sender, RoutedEventArgs e)
+        //{
+        //    Wprowadzanie.Text = "straciłem focus";
+        //}
+
+        private async void PobierzDane_Click(object sender, RoutedEventArgs e)
+        {
+
+            string NazwaZawodnika;
+            NazwaZawodnika = Wprowadzanie.Text;
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api-nba-v1.p.rapidapi.com/players?search={NazwaZawodnika}"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "82e608ca0amshfb8be84d382c693p152ebejsn0ed3121d87c8" },
+        { "X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                var Wczytywanie = JsonConvert.DeserializeObject<Root>(body);
+                if (Wczytywanie.response == null )
+                {
+                    ErrorWindow errorWindow = new ErrorWindow();
+
+                    errorWindow.Show();
+                }
+
+
+                else
+                {
+                    ImięZawodnika.Text = Wczytywanie?.response[0]?.firstname;
+                    NazwiskoZawodnika.Text = Wczytywanie?.response[0]?.lastname;
+                    WagaZawodnika.Text = Wczytywanie?.response[0]?.weight.kilograms;
+                    WzrostZawodnika.Text = Wczytywanie?.response[0]?.height.meters;
+                    IloscZawodnikow.Text = Wczytywanie?.response.Count.ToString();
+                }
+
+
+            }
+        }
+        private void CenterWindowOnScreen()
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
+        private void testokna_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorWindow errorWindow = new ErrorWindow();
+            errorWindow.Show();
+            
+        }
+    }
+}
